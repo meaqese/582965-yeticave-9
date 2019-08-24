76e1c83f1afbe5ab1470443698e267e206943565
meaqese <meaqese@protonmail.com>
2019-08-24 10:24:56 +0300

    m7-t1

--- templates/sign-up-template.php
+++ templates/sign-up-template.php
@@ -1,3 +1,11 @@
+<?php
+    $form = [];
+
+    $form['email'] = $user['email'] ?? '';
+    $form['password'] = $user['password'] ?? '';
+    $form['name'] = $user['name'] ?? '';
+    $form['contact'] = $user['contact'] ?? '';
+?>
 <main>
     <nav class="nav">
         <ul class="nav__list container">
@@ -8,26 +16,26 @@
             <?php endforeach; ?>
         </ul>
     </nav>
-    <form class="form container" action="https://echo.htmlacademy.ru" method="post" autocomplete="off"> <!-- form--invalid -->
+    <form class="form container <?= (isset($error)) ? 'form--invalid' : ''; ?>" action="" method="post" autocomplete="off"> <!-- form--invalid -->
         <h2>Регистрация нового аккаунта</h2>
-        <div class="form__item"> <!-- form__item--invalid -->
+        <div class="form__item <?= (isset($error['email'])) ? 'form__item--invalid' : ''; ?>"> <!-- form__item--invalid -->
             <label for="email">E-mail <sup>*</sup></label>
-            <input id="email" type="text" name="email" placeholder="Введите e-mail">
-            <span class="form__error">Введите e-mail</span>
+            <input id="email" type="text" name="user[email]" placeholder="Введите e-mail" value="<?= htmlspecialchars($form['email']); ?>">
+            <span class="form__error">Введите ваш реальный E-Mail адрес</span>
         </div>
-        <div class="form__item">
+        <div class="form__item <?= (isset($error['password'])) ? 'form__item--invalid' : ''; ?>">
             <label for="password">Пароль <sup>*</sup></label>
-            <input id="password" type="password" name="password" placeholder="Введите пароль">
-            <span class="form__error">Введите пароль</span>
+            <input id="password" type="password" name="user[password]" placeholder="Введите пароль" value="<?= htmlspecialchars($form['password']); ?>">
+            <span class="form__error">Введите пароль который не менее 8 символов</span>
         </div>
-        <div class="form__item">
+        <div class="form__item <?= (isset($error['name'])) ? 'form__item--invalid' : ''; ?>">
             <label for="name">Имя <sup>*</sup></label>
-            <input id="name" type="text" name="name" placeholder="Введите имя">
+            <input id="name" type="text" name="user[name]" placeholder="Введите имя" value="<?= htmlspecialchars($form['name']); ?>">
             <span class="form__error">Введите имя</span>
         </div>
-        <div class="form__item">
+        <div class="form__item <?= (isset($error['contact'])) ? 'form__item--invalid' : ''; ?>">
             <label for="message">Контактные данные <sup>*</sup></label>
-            <textarea id="message" name="message" placeholder="Напишите как с вами связаться"></textarea>
+            <textarea id="message" name="user[contact]" placeholder="Напишите как с вами связаться"><?= htmlspecialchars($form['contact']); ?></textarea>
             <span class="form__error">Напишите как с вами связаться</span>
         </div>
         <span class="form__error form__error--bottom">Пожалуйста, исправьте ошибки в форме.</span>
